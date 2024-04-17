eef1b1485876589cb5b13b2346766087100efb85
sanduni-sc <sanduni@saras.care>
2024-04-17 14:22:44 +1000

    Update settings.py

--- sarascare_py/settings.py
+++ sarascare_py/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-63gddvop@%n4)hhk$-^5+he%#jdd6o(#rqa-mq5jsv#^esuukw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True    
+DEBUG = False    
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pysarascare.azurewebsites.net']
 
 # Application definition
 
